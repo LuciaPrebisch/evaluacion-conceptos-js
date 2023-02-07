6fde0d007294933e8f3d0aa02e48cbde4544987e
Lucia Prebisch <luciaprebisch10@gmail.com>
2023-02-07 19:58:44 -0300

    se agrega el proyecto

---
+++ evaluacion.js
@@ -0,0 +1,13 @@
+/* Evaluación de conocimientos básicos en JS.
+
+1) Guarda tu proyecto en tu github con el nombre evaluacion-conceptos-js
+2) Por cada ejercicio harás un commit en donde reflejarás con la descripción el número de ejercicio.
+3) Resolvé usando prompt, alert y condicionales:
+a) Pida al usuario su nombre y muestre un mensaje de bienvenida.
+b) Pida al usuario dos números y muestre su suma.
+c) Pida al usuario dos números y muestre el mayor.
+d) Pida al usuario tres números y muestre una leyenda que diga si los tres son iguales, sino lo son que también lo informe.
+e) Pida al usuario tres números y muéstrelos ordenados de mayor a menor. */
+
+let usuario = prompt ("Ingrese su nombre")
+console.log ("Bienvenida" + usuario)
