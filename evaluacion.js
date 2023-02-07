2830b9d7da7649afd4bc36b6a6bde4ceffb3d4ec
LuciaPrebisch <luciaprebisch@gmail.com>
2023-02-07 20:18:33 -0300

    ejercicio 2

--- evaluacion.js
+++ evaluacion.js
@@ -9,7 +9,12 @@ c) Pida al usuario dos números y muestre el mayor.
 d) Pida al usuario tres números y muestre una leyenda que diga si los tres son iguales, sino lo son que también lo informe.
 e) Pida al usuario tres números y muéstrelos ordenados de mayor a menor. */
 
+// 1
 let usuario = prompt ("Ingrese su nombre")
 console.log ("Bienvenida" + usuario)
 
-let Usuario = prompt ("Ingrese un numero")
+// 2
+let numero1 = parseInt (prompt ("Ingrese un numero"))
+let numero2 = parseInt (prompt ("Ingrese un numero"))
+let resultado = numero1 + numero2 
+console.log(resultado) 
